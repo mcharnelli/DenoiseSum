9e3743a6167d2d2ca99b03a67900d4deeec45c47
Maria Emilia Charnelli <maria.emilia.charnelli@gmail.com>
2021-10-26 15:48:51 +0200

    Update for read dataframe chunks

--- DenoiseSum/LanguageModel/data.py
+++ DenoiseSum/LanguageModel/data.py
@@ -3,26 +3,42 @@ from pathlib import Path
 from DenoiseSum.utils import JSONIterator
 from tqdm.auto import tqdm
 import ijson
+import pandas as pd
+from torch.utils.data import IterableDataset
+import csv
 
-class LanguageModelData:
+
+def string_to_tokens(text: str, word_dict:dict):
+    sos = word_dict['<s>']
+    eos = word_dict['</s>']
+    tokens = text.strip().split()
+    ids = []
+    for token in tokens:
+        if token not in word_dict:
+            ids.append(word_dict["<unk>"])
+        else:
+            ids.append(word_dict[token])
+    return [sos] + ids + [eos]
+
+class LanguageModelData(IterableDataset):
     def __init__(self, file_path: Path, word_dict:dict, review_key:str):
         self.file_path = file_path
-        self.file = open(file_path, "r", encoding="utf-8", errors="ignore")
         self.review_key = review_key
         self.word_dict = word_dict
+        self.length =sum(1 for line in csv.reader(open(self.file_path))) - 1
 
     def __iter__(self):
-        self.iterator =  ijson.items(self.file, "", multiple_values=True)
+        self.iterator = pd.read_csv(self.file_path, chunksize=1)
         return self
 
     def __next__(self):
-        data = next(self.iterator)
-        ids = []
-        tokens = data[self.review_key].strip().split()
-        for token in tokens:
-            if token not in self.word_dict:
-                ids.append(self.word_dict["<unk>"])
-            else:
-                ids.append(self.word_dict[token])
-        return ids
 
+        data = next(self.iterator)
+        while(data[self.review_key].isna().all()):
+            data = next(self.iterator)
+        
+        text = data[self.review_key].iloc[0]
+        return string_to_tokens(text, self.word_dict)
+        
+    def __len__(self):
+        return self.length
