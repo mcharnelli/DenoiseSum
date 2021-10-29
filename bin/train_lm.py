2787ba46a9d7694917433070a7df2a7c3b269b2d
Ubuntu <ubuntu@ip-172-31-35-211.us-east-2.compute.internal>
2021-10-29 19:45:25 +0000

    Update parameters

--- bin/train_lm.py
+++ bin/train_lm.py
@@ -22,11 +22,14 @@ if __name__ == '__main__':
 
   parser.add_argument('--num_epoch', default=20, type=int)
   parser.add_argument('--eval_every', default=2500, type=int)
-  parser.add_argument('--stop_after', default=40, type=int)
+  parser.add_argument('--stop_after', default=5, type=int)
 
   parser.add_argument('--train_file', default='reviews_large_train.csv', type=str)
   parser.add_argument('--dev_file', default='reviews_large_validation.csv', type=str)
   parser.add_argument('--test_file', default='reviews_large_test.csv', type=str)
+
+  parser.add_argument('--replace_word_dict', default='False', type=str)
+  parser.add_argument('--retrain_model', default='False', type=str)
 
   parser.add_argument('--model_file', default='lm.model', type=str)
   parser.add_argument('--sos', default=2, type=int)
@@ -38,6 +41,16 @@ if __name__ == '__main__':
   args.train_file = DATA_PATH / args.dataset / args.train_file
   args.dev_file =  DATA_PATH / args.dataset / args.dev_file
   args.test_file = DATA_PATH / args.dataset / args.test_file
+
+  if args.replace_word_dict == 'False':
+    args.replace_word_dict = False
+  else:
+     args.replace_word_dict = True
+
+  if args.retrain_model == 'False':
+    args.retrain_model = False
+  else:
+     args.retrain_model = True
 
   (DATA_PATH / 'model' / args.dataset).mkdir(parents = True, exist_ok=True)
   args.model_file = DATA_PATH / 'model' / args.dataset / args.model_file 
